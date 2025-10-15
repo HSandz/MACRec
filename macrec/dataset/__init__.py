d84f9ac9e02fe5baeac2fc38d8e4751dd3228680
Thanh Hải <optimus1072005@gmail.com>
2025-10-15 10:57:22 +0700

    checkpoint

--- macrec/dataset/__init__.py
+++ macrec/dataset/__init__.py
@@ -1,3 +1,2 @@
 from macrec.dataset.ml100k import process_data as ml100k_process_data, process_data_with_retrieval as ml100k_process_data_with_retrieval
 from macrec.dataset.amazon import process_data as amazon_process_data, process_data_with_retrieval as amazon_process_data_with_retrieval
-from macrec.dataset.ml1m import process_data as ml1m_process_data, process_data_with_retrieval as ml1m_process_data_with_retrieval
