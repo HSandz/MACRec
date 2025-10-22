86e56552abac00e9a72a4fd13db1e057f9c90077
Thanh Hải <optimus1072005@gmail.com>
2025-10-22 14:05:16 +0700

    test(data): add Gowalla and remove ML-1M

--- macrec/dataset/__init__.py
+++ macrec/dataset/__init__.py
@@ -1,3 +1,4 @@
 from macrec.dataset.ml100k import process_data as ml100k_process_data
 from macrec.dataset.amazon import process_data as amazon_process_data
 from macrec.dataset.ml1m import process_data as ml1m_process_data
+from macrec.dataset.gowalla import process_data as gowalla_process_data
