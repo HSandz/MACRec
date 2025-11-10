36b691cc151eadca71df361130369f65ee836702
Sand <lehoangsand55@gmail.com>
2025-11-10 19:54:47 +0700

    fix

--- macrec/tasks/__init__.py
+++ macrec/tasks/__init__.py
@@ -3,9 +3,7 @@ from macrec.tasks.preprocess import PreprocessTask
 from macrec.tasks.generate_embedding_test import GenerateEmbeddingTestTask
 from macrec.tasks.sample import SampleTask
 from macrec.tasks.calculate import CalculateTask
-from macrec.tasks.reward_update import RewardUpdateTask
 
 from macrec.tasks.pure_generation import PureGenerationTask as GenerationTask, TestGenerationTask
 from macrec.tasks.evaluate import EvaluateTask
 from macrec.tasks.test import TestTask
-from macrec.tasks.feedback import FeedbackTask
